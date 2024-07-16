202f81fac23be6fc706e74f62f6cb95f875d6bcb
KhageshorGiri <girikhageshor2432@gmail.com>
2024-07-16 21:23:45 +0545

    Added Base Exception Class

---
+++ Packing_IT/src/PackingIT.Domain/Entities/PackingItem.cs
@@ -0,0 +1,17 @@
+﻿namespace PackingIT.Domain.Entities;
+
+public  class PackingItem
+{
+    public Guid Id { get; set; }
+
+    private string? _name;
+    private string? _localization;
+
+
+    public readonly LinkedList<PackingItem> _items = new();
+
+    public void AddItem(PackingItem item)
+    {
+        _items.AddLast(item);
+    }
+}
---
+++ Packing_IT/src/shared/PackingIT.Shared.Abstraction/Exceptions/PackingITException.cs
@@ -0,0 +1,7 @@
+﻿
+namespace PackingIT.Shared.Abstraction.Exceptions;
+
+public abstract class PackingITException : Exception
+{
+    protected PackingITException(string message) : base(message) { }
+}
