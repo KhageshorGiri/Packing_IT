39676068baa22b88c2b7a358d9542c9868b9b199
KhageshorGiri <girikhageshor2432@gmail.com>
2024-07-17 21:15:06 +0545

    Added Exceptuion and Value object for PackingItemId

---
+++ Packing_IT/src/PackingIT.Domain/Excepitons/EmptyPackingListIdException.cs
@@ -0,0 +1,7 @@
+﻿using PackingIT.Shared.Abstraction.Exceptions;
+
+namespace PackingIT.Domain.Excepitons;
+public class EmptyPackingListIdException : PackingITException
+{
+    public EmptyPackingListIdException() : base("Packing List ID cannot be empty.") { }
+}
--- Packing_IT/src/PackingIT.Domain/ValueObjects/PackingListId.cs
+++ Packing_IT/src/PackingIT.Domain/ValueObjects/PackingListId.cs
@@ -1,6 +1,20 @@
 ﻿
+using PackingIT.Domain.Excepitons;
+
 namespace PackingIT.Domain.ValueObjects;
 
 public record PackingListId
 {
+    public Guid Value { get; }
+
+    public PackingListId(Guid value)
+    {
+        if(value == Guid.Empty)
+            throw new EmptyPackingListIdException();
+        Value = value;
+    }
+
+    public static implicit operator PackingListId(Guid id) => new PackingListId(id);
+
+    public static implicit operator Guid(PackingListId id) => id.Value;
 }
