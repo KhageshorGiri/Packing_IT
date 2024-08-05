7c3129e458a37edd9fc1ca2886c5387fe5436f0f
KhageshorGiri <girikhageshor2432@gmail.com>
2024-08-05 20:47:20 +0545

    Added Ploicy for temprature

---
+++ Packing_IT/src/PackingIT.Domain/Policies/Temperature/LowTemperaturePolicy.cs
@@ -0,0 +1,20 @@
+﻿using PackingIT.Domain.ValueObjects;
+
+namespace PackingIT.Domain.Policies.Temperature;
+
+internal sealed class LowTemperaturePolicy : IPackingItemsPolicy
+{
+    public bool IsApplicable(PolicyData data)
+          => data.Temperature < 100;
+
+    public IEnumerable<PackingItem> GenerateItems(PolicyData data)
+        => new List<PackingItem>
+        {
+            new("Winter hat", 1),
+            new("Scarf", 1),
+            new("Gloves", 1),
+            new("Hoodie", 1),
+            new("Warm jacket", 1),
+        };
+
+}
