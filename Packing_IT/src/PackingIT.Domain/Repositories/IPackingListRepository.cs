9522194b80997e56b0fdbe1d27d8a48d20a21899
KhageshorGiri <girikhageshor2432@gmail.com>
2024-08-05 21:10:00 +0545

    Added Repoistories for domain

---
+++ Packing_IT/src/PackingIT.Domain/Repositories/IPackingListRepository.cs
@@ -0,0 +1,12 @@
+﻿using PackingIT.Domain.Entities;
+using PackingIT.Domain.ValueObjects;
+
+namespace PackingIT.Domain.Repositories;
+
+public interface IPackingListRepository
+{
+    Task<PackingList> GetAsync(PackingListId id);
+    Task AddAsync(PackingList packingList);
+    Task UpdateAsync(PackingList packingList);
+    Task DeleteAsync(PackingList packingList);
+}
