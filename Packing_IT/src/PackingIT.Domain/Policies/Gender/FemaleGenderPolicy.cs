f11a254951038695bea7bad6f531489b196ae4aa
KhageshorGiri <girikhageshor2432@gmail.com>
2024-08-05 20:43:23 +0545

    Added Male and Femail Policies

---
+++ Packing_IT/src/PackingIT.Domain/Policies/Gender/FemaleGenderPolicy.cs
@@ -0,0 +1,18 @@
+﻿using PackingIT.Domain.ValueObjects;
+
+namespace PackingIT.Domain.Policies.Gender;
+
+internal sealed class FemaleGenderPolicy : IPackingItemsPolicy
+{
+    public bool IsApplicable(PolicyData data)
+        => data.Gender is Enums.Gender.Female;
+
+    public IEnumerable<PackingItem> GenerateItems(PolicyData data)
+        => new List<PackingItem>
+        {
+            new("Lipstick", 1),
+            new("Powder", 1),
+            new("Eyeliner", 1)
+        };
+
+}
