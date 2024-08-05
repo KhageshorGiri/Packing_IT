e0b64e2d3d759567bfc59b4716d59118b6595594
KhageshorGiri <girikhageshor2432@gmail.com>
2024-08-05 20:37:10 +0545

    Added Basic Policy contract

---
+++ Packing_IT/src/PackingIT.Domain/Policies/IPackingItemsPolicy.cs
@@ -0,0 +1,9 @@
+﻿using PackingIT.Domain.ValueObjects;
+
+namespace PackingIT.Domain.Policies;
+
+public interface IPackingItemsPolicy
+{
+    bool IsApplicable(PolicyData data);
+    IEnumerable<PackingItem> GenerateItems(PolicyData data);
+}
---
+++ Packing_IT/src/PackingIT.Domain/Policies/PolicyData.cs
@@ -0,0 +1,8 @@
+﻿
+using PackingIT.Domain.Enums;
+using PackingIT.Domain.ValueObjects;
+
+namespace PackingIT.Domain.Policies;
+
+public record PolicyData(TravelDays Days, Gender Gender, Temperature Temperature, Localization Localization);
+
