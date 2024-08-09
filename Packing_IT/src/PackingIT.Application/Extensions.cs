c6e883b66a53b027dfcd5403c10a34de663d130e
KhageshorGiri <girikhageshor2432@gmail.com>
2024-08-09 21:33:07 +0545

    Added Extension in application layer.

---
+++ Packing_IT/src/PackingIT.Application/Extensions.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using PackingIT.Domain.Factories;
+using PackingIT.Domain.Policies;
+using PackingIT.Shared.Commands;
+
+namespace PackingIT.Application;
+
+public static class Extensions
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        services.AddCommands();
+        services.AddSingleton<IPackingListFactory, PackingListFactory>();
+
+        services.Scan(b => b.FromAssemblies(typeof(IPackingItemsPolicy).Assembly)
+            .AddClasses(c => c.AssignableTo<IPackingItemsPolicy>())
+            .AsImplementedInterfaces()
+            .WithSingletonLifetime());
+
+        return services;
+    }
+}
